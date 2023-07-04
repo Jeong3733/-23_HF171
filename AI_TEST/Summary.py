f522d6a43c82ec1c0b86c882e58949fb61ba9a8f
TaekGwan Kim <sbe07032@naver.com>
2023-07-04 21:32:58 +0900

    [Update] Document Summary AI ADD

--- AI_TEST/Summary.py
+++ AI_TEST/Summary.py
@@ -4,12 +4,14 @@
 # https://colab.research.google.com/drive/1gyGZn_LZNrYXYXa-pltFExbptIe7DAPe?usp=sharing
 
 
-from langchain import OpenAI, PromptTemplate, LLMChain
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
-from langchain.chains.mapreduce import MapReduceChain
+from langchain import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter # CharacterTextSplitter
+
 from langchain.prompts import PromptTemplate
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.chains.summarize import load_summarize_chain
+
+# import textwrap
 
 import os
 from dotenv import load_dotenv
@@ -17,7 +19,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class QAChain:
+class SummaryChain:
     def __init__(self) -> None:
         pass
     
@@ -31,20 +33,41 @@ class QAChain:
         texts = text_splitter.split_documents(documents)
         
         llm = OpenAI(temperature=0)
-        
-        import textwrap
-
         chain = load_summarize_chain(llm, 
                                     chain_type="map_reduce",
                                     verbose = False)
 
         output_summary = chain.run(texts)
         print(output_summary)
+    
+    def run2(self):
+        path = '/Users/ktg/Desktop/23_HF171/AI_TEST/data/서울특별시 버스노선 혼잡도 예측을 통한 다람쥐버스 신규 노선제안(장려).pdf'
+        loader = PyMuPDFLoader(path)
+        documents = loader.load()
+        
+        # splitting the text into
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        texts = text_splitter.split_documents(documents)
+        
+        prompt_template = """Write a concise bullet point summary of the following:
+        {text}
+
+        CONSCISE SUMMARY IN BULLET POINTS:"""
+
+        BULLET_POINT_PROMPT = PromptTemplate(template=prompt_template,
+                                input_variables=["text"])
+
+        llm = OpenAI(temperature=0)        
+        chain = load_summarize_chain(llm, 
+                                    chain_type="stuff", 
+                                    prompt=BULLET_POINT_PROMPT)
+
+        output_summary = chain.run(texts)
         
 
 def main():
-    qa = QAChain()
-    qa.run()
+    summarychain = SummaryChain()
+    summarychain.run2()
     
 if __name__ == '__main__':
     main()
