4e04076b7a12567e9a878e443955fc7458f4ce2c
TaekGwan Kim <sbe07032@naver.com>
2023-07-11 00:31:48 +0900

    [Update] AI 기능 업데이트

--- AI_TEST/ai.py
+++ AI_TEST/ai.py
@@ -18,36 +18,36 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class AI:
     def __init__(self) -> None:
-        self.file_path = None
-        self.chunk_size = 1000
-        self.chunk_overlap = 200
-        
         self.embedding = OpenAIEmbeddings()
-        self.persist_directory = None
-        self.n_similar = 3
-    
+
     def _upload_document(self):
         """
         문서를 업로드하면 텍스트 리스트로 변환하는 함수
         """
-        loader = PyMuPDFLoader(file_path=self.file_path)
+        file_path = None
+        chunk_size = 1000
+        chunk_overlap = 200
+
+        loader = PyMuPDFLoader(file_path=file_path)
         documents = loader.load()
-        
+
         # splitting the text into
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
-        texts = text_splitter.split_documents(documents)
-        return texts
+            chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        docs = text_splitter.split_documents(documents)
+        return docs
 
-    def _get_vectordb(self, texts):
+    def _get_vectordb(self, docs):
         """
         텍스트를 기존(새로운) 벡터 DB에 저장하는 함수
         """
-        vectordb = Chroma.from_documents(documents=texts, 
-                                        embedding=self.embedding,
-                                        persist_directory=self.persist_directory)
+        persist_directory = None
+        vectordb = Chroma.from_documents(documents=docs,
+                                         embedding=self.embedding,
+                                         persist_directory=persist_directory)
         vectordb.persist()
         return vectordb
 
@@ -55,19 +55,24 @@ class AI:
         """
         _load_retriever 주어진 텍스트와 유사한 상위 N개의 벡터를 반환하는 함수
         """
-        vectordb = Chroma(persist_directory=self.persist_directory, 
-                        embedding_function=self.embedding)
+        persist_directory = None
+        n_similar = 3
 
-        retriever = vectordb.as_retriever(search_kwargs={"k": self.k_similar})
+        vectordb = Chroma(persist_directory=persist_directory,
+                          embedding_function=self.embedding)
+
+        retriever = vectordb.as_retriever(search_kwargs={"k": n_similar})
         return retriever
 
     def _similarity_search(self, text):
         """
         주어진 텍스트와 유사한 상위 N개의 벡터를 반환하는 함수
         """
-        vectordb = Chroma(persist_directory=self.persist_directory, 
-                        embedding_function=self.embedding)
-        
+        persist_directory = None
+
+        vectordb = Chroma(persist_directory=persist_directory,
+                          embedding_function=self.embedding)
+
         # query it
         docs = vectordb.similarity_search(text)
         return docs
@@ -86,27 +91,40 @@ class AI:
         res = prompt
         return res
 
+
+class DoucmentInit(AI):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def run(self):
+        docs = self._upload_document()
+        vectordb = self._get_vectordb(docs)
+        self._similarity_search()
+        self._load_retriever()
+
+
 class Summary(AI):
     def __init__(self) -> None:
         super().__init__()
-    
+
     def run(self, file_path):
         texts = self._upload_document(file_path=file_path)
-        
+
         llm = OpenAI(temperature=0)
-        chain = load_summarize_chain(llm, 
-                             chain_type="map_reduce",
-                             verbose = False)
+        chain = load_summarize_chain(llm,
+                                     chain_type="map_reduce",
+                                     verbose=False)
         output_summary = chain.run(texts)
         return output_summary
+
 
 class DocumentQA(AI):
     def __init__(self) -> None:
         super().__init__()
-    
+
     def newinit(self, file_path):
         texts = self._upload_document(file_path=file_path)
         vectordb = self._get_vectordb(texts)
-    
+
     def question(self, query):
         self._get_retriever()
