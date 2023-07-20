d66b896822701332288e0b0f4e54302ee246d8aa
TaekGwan Kim <sbe07032@naver.com>
2023-07-20 21:56:49 +0900

    [Update] 2.0 공모전 상세

--- geeks-react-2.1.1/src/components/marketing/pages/courses/add-new-course/steps/BasicInformation.js
+++ geeks-react-2.1.1/src/components/marketing/pages/courses/add-new-course/steps/BasicInformation.js
@@ -1,100 +1,154 @@
 // import node module libraries
-import { Card, Form, Button } from 'react-bootstrap';
+import { Card, Form, Button } from "react-bootstrap";
+// import React, { useState, Fragment } from "react";
 
 // import custom components
-import { FormSelect } from 'components/elements/form-select/FormSelect';
-import ReactQuillEditor from 'components/elements/editor/ReactQuillEditor';
+import { FormSelect } from "components/elements/form-select/FormSelect";
+import ReactQuillEditor from "components/elements/editor/ReactQuillEditor";
 
 const BasicInformation = (props) => {
-	const { next } = props;
+  const { next, data, handleChange } = props;
 
-	const categoryOptions = [
-		{ value: 'React', label: 'React' },
-		{ value: 'Javascript', label: 'Javascript' },
-		{ value: 'HTML', label: 'HTML' },
-		{ value: 'Vuejs', label: 'Vue js' },
-		{ value: 'Gulpjs', label: 'Gulp js' }
-	];
+  const CompetitionType = [
+    { value: "분류1", label: "분류1" },
+    { value: "분류2", label: "분류2" },
+    { value: "분류3", label: "분류3" },
+    { value: "분류4", label: "분류4" },
+    { value: "분류5", label: "분류5" },
+    { value: "분류6", label: "분류6" },
+    { value: "분류7", label: "분류7" },
+  ];
 
-	const CoursesLevel = [
-		{ value: 'Intermediate', label: 'Intermediate' },
-		{ value: 'Beignners', label: 'Beignners' },
-		{ value: 'Advance', label: 'Advance' }
-	];
+  // const initialValue = `<p>Insert course description</p>
+  //                     <p><br /></p>
+  //                     <p>Some initial <strong>bold</strong> text</p>
+  //                     <p><br /></p><p><br /></p><p><br /></p><p><br /></p>`;
 
-	const initialValue = `<p>Insert course description</p>
-                      <p><br /></p>        
-                      <p>Some initial <strong>bold</strong> text</p>
-                      <p><br /></p><p><br /></p><p><br /></p><p><br /></p>`;
+  return (
+    <Form>
+      {/* Card */}
+      <Card className="mb-3 ">
+        <Card.Header className="border-bottom px-4 py-3">
+          <h4 className="mb-0">공모전 정보</h4>
+        </Card.Header>
+        {/* Card body */}
+        <Card.Body>
+          {/* 공모전 이름  */}
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="competition_name">공모전 이름</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Course Title"
+              id="competition_name"
+              name="competition_name"
+              value={data.competition_name}
+              onChange={handleChange}
+            />
+            <Form.Text className="text-muted">
+              Write a 60 character course title.
+            </Form.Text>
+          </Form.Group>
 
-	return (
-		<Form>
-			{/* Card */}
-			<Card className="mb-3 ">
-				<Card.Header className="border-bottom px-4 py-3">
-					<h4 className="mb-0">Basic Information</h4>
-				</Card.Header>
-				{/* Card body */}
-				<Card.Body>
-					{/* Title  */}
-					<Form.Group className="mb-3">
-						<Form.Label htmlFor="courseTitle">Course Title</Form.Label>
-						<Form.Control
-							type="text"
-							placeholder="Course Title"
-							id="course_title"
-							name="course_title"
-						/>
-						<Form.Text className="text-muted">
-							Write a 60 character course title.
-						</Form.Text>
-					</Form.Group>
+          {/* 공모전 분류 */}
+          <Form.Group className="mb-3">
+            <Form.Label>공모전 분류</Form.Label>
+            <FormSelect
+              options={CompetitionType}
+              id="competition_type"
+              name="competition_type"
+              placeholder="Select Type"
+              value={data.competition_type}
+              defaultselected={data.competition_type}
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-					{/* Category */}
-					<Form.Group className="mb-3">
-						<Form.Label>Category category</Form.Label>
-						<FormSelect
-							options={categoryOptions}
-							id="category_category"
-							name="category_category"
-							placeholder="Select Category"
-						/>
-						<Form.Text className="text-muted">
-							Help people find your courses by choosing categories that
-							represent your course.
-						</Form.Text>
-					</Form.Group>
+          {/* 공모전 요약 설명 */}
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="competition_description">
+              공모전 요약 설명
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Course Title"
+              id="competition_description"
+              name="competition_description"
+              value={data.competition_description}
+              onChange={handleChange}
+            />
+            <Form.Text className="text-muted">
+              Write a 60 character course title.
+            </Form.Text>
+          </Form.Group>
 
-					{/* Courses level */}
-					<Form.Group className="mb-3">
-						<Form.Label>Courses level</Form.Label>
-						<FormSelect
-							options={CoursesLevel}
-							id="courses_level"
-							name="courses_level"
-							placeholder="Select level"
-						/>
-					</Form.Group>
+          {/* Cover image */}
+          <Form.Label>Cover image</Form.Label>
+          <Form.Group className="mb-1 input-group">
+            <Form.Control
+              id="inputfavicon"
+              name="competition_image"
+              type="file"
+              className="form-control"
+              // value={data.competition_image}
+              onChange={handleChange}
+            />
+            <Form.Label
+              htmlFor="competition_image"
+              className="input-group-text mb-0"
+            >
+              Upload
+            </Form.Label>
+            <Form.Text className="text-muted">
+              공모전 이미지를 업로드하세요. Important guidelines: 750x440
+              pixels; .jpg, .jpeg,. gif, or .png. no text on the image.
+            </Form.Text>
+          </Form.Group>
 
-					{/* Course Description*/}
-					<Form.Group className="mb-3">
-						<Form.Label>Course Description</Form.Label>
-						<ReactQuillEditor
-							initialValue={initialValue}
-							id="course_description"
-							name="course_description"
-						/>
-						<Form.Text className="text-muted">
-							A brief summary of your courses.
-						</Form.Text>
-					</Form.Group>
-				</Card.Body>
-			</Card>
-			{/* Button */}
-			<Button variant="primary" onClick={next}>
-				Next
-			</Button>
-		</Form>
-	);
+          {/* File upload */}
+          <Form.Label>File upload</Form.Label>
+          <Form.Group className="mb-1 input-group">
+            <Form.Control
+              id="inputfavicon"
+              name="competition_docs"
+              type="file"
+              className="form-control"
+              // value={data.competition_docs}
+              onChange={handleChange}
+            />
+            <Form.Label
+              htmlFor="competition_docs"
+              className="input-group-text mb-0"
+            >
+              Upload
+            </Form.Label>
+            <Form.Text className="text-muted">
+              공모전 관련 파일 업로드하세요. Important guidelines: 750x440
+              pixels; .jpg, .jpeg,. gif, or .png. no text on the image.
+            </Form.Text>
+          </Form.Group>
+
+          {/* 공모전 소개글*/}
+          <Form.Group className="mb-3">
+            <Form.Label>공모전 소개글</Form.Label>
+            <ReactQuillEditor
+              initialValue={data.competition_intro}
+              id="competition_intro"
+              name="competition_intro"
+              // value={data.competition_intro}
+              handleChange={handleChange}
+            />
+            <Form.Text className="text-muted">
+              A brief summary of your courses.
+            </Form.Text>
+          </Form.Group>
+        </Card.Body>
+      </Card>
+      {/* Button */}
+      <Button variant="primary" onClick={next}>
+        Next
+      </Button>
+    </Form>
+  );
 };
+
 export default BasicInformation;
--- geeks-react-2.1.1/src/layouts/AllRoutes.js
+++ geeks-react-2.1.1/src/layouts/AllRoutes.js
@@ -13,6 +13,12 @@ import DefaultLayout from "layouts/marketing/DefaultLayout";
 import NotFound from "layouts/marketing/NotFound";
 import SignIn from "components/dashboard/authentication/SignIn";
 
+import CompanyAbout from "components/marketing/pages/jobs/company/About";
+import CompanyReviews from "components/marketing/pages/jobs/company/Reviews";
+import CompanyBenifits from "components/marketing/pages/jobs/company/Benifits";
+import CompanyPhotos from "components/marketing/pages/jobs/company/Photos";
+import UploadResume from "components/marketing/pages/jobs/upload-resume/UploadResume";
+
 const AllRoutes = () => {
   return (
     <Routes>
@@ -22,93 +28,81 @@ const AllRoutes = () => {
       <Route element={<DefaultLayout />}>
         {/* <Route path="/" element={<CourseIndex />} /> */}
         <Route path="/" element={<IndexPage />}></Route>
-        <Route
-          path="/add-new-competition/"
-          element={<AddNewCourse />}
-        />
+        <Route path="/add-new-competition/" element={<AddNewCourse />} />
 
         {/* 1.0 전체 공모전 */}
-        <Route
-          path="/explore"
-          element={<CourseFilterPage />}
-        />
+        <Route path="/explore" element={<CourseFilterPage />} />
 
         {/* 2.0 공모전 상세 */}
+        {/* 소개글 */}
+        <Route path="/detail/:competiton_id/" element={<CompanyAbout />} />
         <Route
-          path="/detail/:competiton_id"
-          element={<ComingSoon />}
+          path="/detail/:competiton_id/readme/"
+          element={<CompanyAbout />}
+        />
+        {/* 공지사항 */}
+        <Route
+          path="/detail/:competiton_id/announcements/"
+          element={<CompanyReviews />}
+        />
+        {/* QA 게시판 */}
+        <Route
+          path="/detail/:competiton_id/qna/"
+          element={<CompanyReviews />}
+        />
+
+        {/* 제출 */}
+        <Route
+          path="/detail/:competiton_id/submit/"
+          element={<CompanyPhotos />}
+        />
+
+        <Route
+          path="/detail/:competiton_id/schedules/:schedule_id/"
+          element={<CompanyReviews />}
         />
         <Route
-          path="/detail/:competiton_id/readme"
-          element={<ComingSoon />}
+          path="/detail/:competiton_id/schedules/:schedule_id/readme/"
+          element={<CompanyAbout />}
         />
+        <Route
+          path="/detail/:competiton_id/schedules/:schedule_id/announcements/"
+          element={<CompanyPhotos />}
+        />
+        <Route
+          path="/detail/:competiton_id/schedules/:schedule_id/qna/"
+          element={<CompanyPhotos />}
+        />
+        <Route
+          path="/detail/:competiton_id/schedules/:schedule_id/submit/"
+          element={<UploadResume />}
+        />
+        <Route path="/evaluate/:schedule_id" element={<ComingSoon />} />
         <Route
           path="/detail/:competiton_id/:schedule_id"
           element={<ComingSoon />}
         />
-        <Route
-          path="/detail/:competiton_id/:schedule_id/readme"
-          element={<ComingSoon />}
-        />
-        <Route
-          path="/detail/:competiton_id/:schedule_id/announcements"
-          element={<ComingSoon />}
-        />
-        <Route
-          path="/detail/:competiton_id/:schedule_id/qna"
-          element={<ComingSoon />}
-        />
-        <Route
-          path="/detail/:competiton_id/:schedule_id/submit"
-          element={<ComingSoon />}
-        />
-        <Route
-          path="/evaluate/:schedule_id"
-          element={<ComingSoon />}
-        />
-        <Route
-          path="/detail/:competiton_id/:schedule_id"
-          element={<ComingSoon />}
-        />
-        <Route
-          path="/detail/:competiton_id/qna"
-          element={<ComingSoon />}
-        />
+        <Route path="/detail/:competiton_id/qna" element={<ComingSoon />} />
         <Route
           path="/detail/:competiton_id/announcements"
           element={<ComingSoon />}
         />
-        <Route
-          path="/manage/:competiton_id"
-          element={<ComingSoon />}
-        />
+        <Route path="/manage/:competiton_id" element={<ComingSoon />} />
 
         {/* 3.0 공모전 개설 */}
-        <Route
-          path="/add-new-competition"
-          element={<ComingSoon />}
-        />
+        <Route path="/add-new-competition" element={<ComingSoon />} />
       </Route>
 
       {/* Routes with AnotherLayout */}
       <Route element={<ComingSoon />}>
         {/* 4.0 평가 */}
-        <Route
-          path="/evaluate/:schedule_id"
-          element={<ComingSoon />}
-        />
+        <Route path="/evaluate/:schedule_id" element={<ComingSoon />} />
         <Route
           path="/evaluate/:schedule_id/files/:file_id"
           element={<ComingSoon />}
         />
-        <Route
-          path="/manage/:competiton_id"
-          element={<ComingSoon />}
-        />
-        <Route
-          path="/manage/:competiton_id/readme"
-          element={<ComingSoon />}
-        />
+        <Route path="/manage/:competiton_id" element={<ComingSoon />} />
+        <Route path="/manage/:competiton_id/readme" element={<ComingSoon />} />
         <Route
           path="/manage/:competiton_id/:schedule_id"
           element={<ComingSoon />}
@@ -121,10 +115,7 @@ const AllRoutes = () => {
           path="/manage/:competiton_id/:schedule_id/permissions"
           element={<ComingSoon />}
         />
-        <Route
-          path="/manage/:competiton_id/members"
-          element={<ComingSoon />}
-        />
+        <Route path="/manage/:competiton_id/members" element={<ComingSoon />} />
         <Route
           path="/manage/:competiton_id/announcements"
           element={<ComingSoon />}
@@ -133,22 +124,13 @@ const AllRoutes = () => {
 
       {/* Routes with NotFound */}
       <Route element={<NotFound />}>
-        <Route
-          path="/coming-soon/"
-          element={<ComingSoon />}
-        />
+        <Route path="/coming-soon/" element={<ComingSoon />} />
         <Route path="/404/" element={<Error404 />} />
-        <Route
-          path="/maintenance-mode/"
-          element={<MaintenanceMode />}
-        />
+        <Route path="/maintenance-mode/" element={<MaintenanceMode />} />
       </Route>
-      
+
       {/*Redirect*/}
-      <Route
-        path="*"
-        element={<Navigate to="/404/" replace />}
-      />
+      <Route path="*" element={<Navigate to="/404/" replace />} />
     </Routes>
   );
 };
