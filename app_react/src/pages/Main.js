dd853dd29568c64a8f0c7205f2ffb6f9acb95755
Ubuntu <ubuntu@ip-172-31-7-64.ap-northeast-2.compute.internal>
2023-07-24 16:08:06 +0900

    [Update] JWT

--- app_react/src/pages/Main.js
+++ app_react/src/pages/Main.js
@@ -1,59 +1,62 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import AuthContext from "../components/AuthContext";
-import {apiUtils} from "../components/utils/ApiUtils";
-import {handleLogError} from "../components/utils/ErrorUtils";
-import {Cookies, useCookies} from "react-cookie";
+import { apiUtils } from "../components/utils/ApiUtils";
+import { handleLogError } from "../components/utils/ErrorUtils";
+import { Cookies, useCookies } from "react-cookie";
 
 function Main() {
-    const Auth = useContext(AuthContext);
+  const Auth = useContext(AuthContext);
 
-    const [cookies, setCookie] = useCookies(['refreshToken']);
-    const refreshToken = cookies.refreshToken;
-    const cookie = new Cookies();
+  const [cookies, setCookie] = useCookies(["refreshToken"]);
+  const refreshToken = cookies.refreshToken;
+  const cookie = new Cookies();
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const isLoggedIn = Auth.userIsAuthenticated();
+    setIsLoggedIn(isLoggedIn);
 
-    useEffect(() => {
-        const isLoggedIn = Auth.userIsAuthenticated();
-        setIsLoggedIn(isLoggedIn);
-    }, []);
+    // Auth.getUser() = 신분증 = 로그인을 하고 springboot에서 받아온 Access 토큰
+    apiUtils.getUserInfo(Auth.getUser()).then((response) => {
+      user = response.data;
+    });
+  }, []);
 
-    const handleGetUserInfo = (e) => {
-        e.preventDefault();
+  // const handleGetUserInfo = (e) => {
+  //     e.preventDefault();
 
-        console.log("access" + Auth.getUser().accessToken)
-        console.log("refresh" + cookie.get("refreshToken"));
+  //     console.log("access" + Auth.getUser().accessToken)
+  //     console.log("refresh" + cookie.get("refreshToken"));
 
-        apiUtils.getUserInfo(Auth.getUser())
-            .then(response => {
-                console.log(response.data);
-            })
-            .catch(error => {
-                handleLogError(error);
-            })
-    };
+  //     apiUtils.getUserInfo(Auth.getUser())
+  //         .then(response => {
+  //             console.log(response.data);
+  //         })
+  //         .catch(error => {
+  //             handleLogError(error);
+  //         })
+  // };
 
-    const handleLogout = () => {
-        Auth.userLogout();
-        setIsLoggedIn(false);
-    };
+  const handleLogout = () => {
+    Auth.userLogout();
+    setIsLoggedIn(false);
+  };
 
-    return (
+  return (
+    <div>
+      {isLoggedIn ? (
         <div>
-            {isLoggedIn ? (
-                <div>
-                    <h1>로그인 O</h1>
-                    <h1>Refresh Token : ${refreshToken}</h1>
-                    <button onClick={handleGetUserInfo}>유저 정보 가져오기</button>
-                    <button onClick={handleLogout}>로그아웃</button>
-                </div>
-            ) : (
-                <h1>로그인 X</h1>
-            )}
+          <h1>로그인 O</h1>
+          <h1>Refresh Token : ${refreshToken}</h1>
+          <button onClick={handleGetUserInfo}>유저 정보 가져오기</button>
+          <button onClick={handleLogout}>로그아웃</button>
         </div>
-    );
+      ) : (
+        <h1>로그인 X</h1>
+      )}
+    </div>
+  );
 }
-
 
 export default Main;
--- geeks-react-2.1.1/src/App.js
+++ geeks-react-2.1.1/src/App.js
@@ -1,21 +1,28 @@
 // import node module libraries
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import layouts
-import ScrollToTop from 'layouts/dashboard/ScrollToTop';
-import AllRoutes from 'layouts/AllRoutes';
+import ScrollToTop from "layouts/dashboard/ScrollToTop";
+import AllRoutes from "layouts/AllRoutes";
 
 // import required stylesheet
-import 'simplebar/dist/simplebar.min.css';
-import 'tippy.js/animations/scale.css';
+import "simplebar/dist/simplebar.min.css";
+import "tippy.js/animations/scale.css";
+
+import { AuthProvider } from "./components/AuthContext";
+import { CookiesProvider } from "react-cookie";
 
 function App() {
-	return (
-		<Router>
-			<ScrollToTop />
-			<AllRoutes />
-		</Router>
-	);
+  return (
+    <CookiesProvider>
+      <AuthProvider>
+        <Router>
+          <ScrollToTop />
+          <AllRoutes />
+        </Router>
+      </AuthProvider>
+    </CookiesProvider>
+  );
 }
 
 export default App;
