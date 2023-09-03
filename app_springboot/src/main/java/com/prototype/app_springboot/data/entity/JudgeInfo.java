34a3571a0c22ca09d157c3fb3306bbfc2ba0da7d
Jeong3733 <wjdwodnr445@naver.com>
2023-09-03 19:13:48 +0900

    Back - 심사위원 추가 / 제거 / 조회

--- app_springboot/src/main/java/com/prototype/app_springboot/data/entity/JudgeInfo.java
+++ app_springboot/src/main/java/com/prototype/app_springboot/data/entity/JudgeInfo.java
@@ -2,6 +2,7 @@ package com.prototype.app_springboot.data.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,9 +13,16 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class JudgeInfo {
     @Id
-    private UUID judgeId;
+    @Column(name = "judge_id")
+    private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private PostInfo postInfo;
+
+    @Builder
+    public JudgeInfo(UUID id, PostInfo postInfo) {
+        this.id = id;
+        this.postInfo = postInfo;
+    }
 }
---
+++ app_springboot/src/main/java/com/prototype/app_springboot/service/JudgeService.java
@@ -0,0 +1,69 @@
+package com.prototype.app_springboot.service;
+
+import com.prototype.app_springboot.data.entity.JudgeInfo;
+import com.prototype.app_springboot.data.entity.PostInfo;
+import com.prototype.app_springboot.data.repository.JudgeInfoRepository;
+import com.prototype.app_springboot.data.repository.PostInfoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class JudgeService {
+    private final JudgeInfoRepository judgeInfoRepository;
+    private final PostInfoRepository postInfoRepository;
+
+    public JudgeService(JudgeInfoRepository judgeInfoRepository, PostInfoRepository postInfoRepository) {
+        this.judgeInfoRepository = judgeInfoRepository;
+        this.postInfoRepository = postInfoRepository;
+    }
+
+    @Transactional
+    public void saveJudge(int postId) {
+        UUID uuid = UUID.randomUUID();
+        PostInfo postInfo = postInfoRepository.findById(postId)
+                .orElseThrow(() -> {
+                    log.error("PostId : {}의 파일이 존재하지 않습니다.", postId);
+                    throw new EntityNotFoundException("해당 PostId의 게시글이 존재하지 않습니다.");
+                });
+
+        JudgeInfo judgeInfo = JudgeInfo.builder()
+                .postInfo(postInfo)
+                .id(uuid)
+                .build();
+
+        judgeInfoRepository.save(judgeInfo);
+    }
+
+    @Transactional
+    public void deleteJudge(String judgeId) {
+        UUID uuid = UUID.fromString(judgeId);
+        JudgeInfo judgeInfo = judgeInfoRepository.findById(uuid)
+                .orElseThrow(() -> {
+                    log.error("JudgeId : {}의 파일이 존재하지 않습니다.", judgeId);
+                    throw new EntityNotFoundException("해당 JudgeId의 심사위원이 존재하지 않습니다.");
+                });
+
+        judgeInfoRepository.delete(judgeInfo);
+    }
+
+    @Transactional
+    public JudgeInfo getJudgeInfoByJudgeId(String judgeId) {
+        UUID uuid = UUID.fromString(judgeId);
+        return judgeInfoRepository.findById(uuid)
+                .orElseThrow(() -> {
+                    log.error("JudgeId : {}의 심사위원이 존재하지 않습니다.", judgeId);
+                    throw new EntityNotFoundException("해당 JudgeId의 심사위원이 존재하지 않습니다.");
+                });
+    }
+
+    @Transactional
+    public List<JudgeInfo> getAllJudgeInfoByPostId(int postId) {
+        return judgeInfoRepository.findAllByPostInfoId(postId);
+    }
+}
