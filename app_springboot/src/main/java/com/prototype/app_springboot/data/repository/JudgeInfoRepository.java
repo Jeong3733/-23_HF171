ec09fdcda7e145ec0b5c9bb135ea35ff0672270e
Jeong3733 <wjdwodnr445@naver.com>
2023-09-05 01:40:21 +0900

    Back - judge 부분 수정 및 완료

--- app_springboot/src/main/java/com/prototype/app_springboot/data/repository/JudgeInfoRepository.java
+++ app_springboot/src/main/java/com/prototype/app_springboot/data/repository/JudgeInfoRepository.java
@@ -4,8 +4,10 @@ import com.prototype.app_springboot.data.entity.JudgeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JudgeInfoRepository extends JpaRepository<JudgeInfo, UUID> {
     List<JudgeInfo> findAllByPostInfoId(int postId);
+    Boolean existsByIdAndPostInfoId(UUID judgeId, int postId);
 }
