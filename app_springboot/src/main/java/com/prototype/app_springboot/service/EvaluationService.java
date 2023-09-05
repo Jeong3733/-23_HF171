def50aa613c83790191d54983bdfc76d79e00d85
Jeong3733 <wjdwodnr445@naver.com>
2023-09-05 18:08:00 +0900

    Back - evalScore add update 합침

--- app_springboot/src/main/java/com/prototype/app_springboot/service/EvaluationService.java
+++ app_springboot/src/main/java/com/prototype/app_springboot/service/EvaluationService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -31,9 +32,9 @@ public class EvaluationService {
 
     @Transactional
     public void addEvaluation(ReqAddEvaluation reqAddEvaluation) {
-        PostInfo postInfo = postInfoRepository.findById(reqAddEvaluation.getPost_id())
+        PostInfo postInfo = postInfoRepository.findById(reqAddEvaluation.getPostId())
                 .orElseThrow(() -> {
-                    log.error("PostId : {}의 파일이 존재하지 않습니다.", reqAddEvaluation.getPost_id());
+                    log.error("PostId : {}의 파일이 존재하지 않습니다.", reqAddEvaluation.getPostId());
                     throw new EntityNotFoundException("해당 PostId의 게시글이 존재하지 않습니다.");
                 });
 
@@ -103,67 +104,28 @@ public class EvaluationService {
                         throw new EntityNotFoundException("해당 UserId의 사용자가 존재하지 않습니다.");
                     });
 
-            EvaluationScore evaluationScore = evaluationScoreRepository.findByPostInfoIdAndEvaluationInfoIdAndJudgeInfoId(postId, evalId, judgeId)
-                    .orElseThrow(() -> {
-                        log.error("PostId : {}, EvalId : {}, JudgeId : {}의 평가점수가 존재하지 않습니다.", postId, evalId, judgeId);
-                        throw new EntityNotFoundException("해당 PostId, EvalId, JudgeId의 평가점수가 존재하지 않습니다.");
-                    });
+            Optional<EvaluationScore> evaluationScoreOptional = evaluationScoreRepository.findByPostInfoIdAndEvaluationInfoIdAndJudgeInfoId(postId, evalId, judgeId);
 
-            EvaluationScore newEvaluationScore = EvaluationScore.builder()
-                    .postInfo(postInfo)
-                    .evaluationInfo(evaluationInfo)
-                    .judgeInfo(judgeInfo)
-                    .userInfo(userInfo)
-                    .score(evaluationScoreDto.getScore())
-                    .comment(evaluationScoreDto.getComment())
-                    .build();
+            if (evaluationScoreOptional.isEmpty()) {
+                EvaluationScore evaluationScore = EvaluationScore.builder()
+                        .postInfo(postInfo)
+                        .evaluationInfo(evaluationInfo)
+                        .judgeInfo(judgeInfo)
+                        .userInfo(userInfo)
+                        .score(evaluationScoreDto.getScore())
+                        .comment(evaluationScoreDto.getComment())
+                        .build();
 
-            evaluationScoreRepository.save(newEvaluationScore);
-        });
-    }
-
-    @Transactional
-    public void updateAllScoreList(List<EvaluationScoreDto> evaluationScoreDtoList) {
-        evaluationScoreDtoList.forEach(evaluationScoreDto -> {
-            int postId = evaluationScoreDto.getPost_id();
-            PostInfo postInfo = postInfoRepository.findById(postId)
-                    .orElseThrow(() -> {
-                        log.error("PostId : {}의 게시글이 존재하지 않습니다.", postId);
-                        throw new EntityNotFoundException("해당 PostId의 게시글이 존재하지 않습니다.");
-                    });
-
-            int evalId = evaluationScoreDto.getEvaluation_id();
-            EvaluationInfo evaluationInfo = evaluationInfoRepository.findById(evalId)
-                    .orElseThrow(() -> {
-                        log.error("EvalId : {}의 평가항목이 존재하지 않습니다.", evalId);
-                        throw new EntityNotFoundException("해당 EvalId의 평가항목이 존재하지 않습니다.");
-                    });
-
-            UUID judgeId = evaluationScoreDto.getJudge_id();
-            JudgeInfo judgeInfo = judgeInfoRepository.findById(judgeId)
-                    .orElseThrow(() -> {
-                        log.error("JudgeId : {}의 심사위원이 존재하지 않습니다.", judgeId);
-                        throw new EntityNotFoundException("해당 JudgeId의 심사위원이 존재하지 않습니다.");
-                    });
-
-            UserInfo userInfo = userInfoRepository.findById(evaluationScoreDto.getUser_id())
-                    .orElseThrow(() -> {
-                        log.error("UserId : {}의 사용자가 존재하지 않습니다.", evaluationScoreDto.getUser_id());
-                        throw new EntityNotFoundException("해당 UserId의 사용자가 존재하지 않습니다.");
-                    });
-
-            EvaluationScore evaluationScore = evaluationScoreRepository.findByPostInfoIdAndEvaluationInfoIdAndJudgeInfoId(postId, evalId, judgeId)
-                    .orElseThrow(() -> {
-                        log.error("PostId : {}, EvalId : {}, JudgeId : {}의 평가점수가 존재하지 않습니다.", postId, evalId, judgeId);
-                        throw new EntityNotFoundException("해당 PostId, EvalId, JudgeId의 평가점수가 존재하지 않습니다.");
-                    });
-
-            evaluationScore.setPostInfo(postInfo);
-            evaluationScore.setEvaluationInfo(evaluationInfo);
-            evaluationScore.setJudgeInfo(judgeInfo);
-            evaluationScore.setUserInfo(userInfo);
-            evaluationScore.setScore(evaluationScoreDto.getScore());
-            evaluationScore.setComment(evaluationScoreDto.getComment());
+                evaluationScoreRepository.save(evaluationScore);
+            } else {
+                EvaluationScore evaluationScore = evaluationScoreOptional.get();
+                evaluationScore.setPostInfo(postInfo);
+                evaluationScore.setEvaluationInfo(evaluationInfo);
+                evaluationScore.setJudgeInfo(judgeInfo);
+                evaluationScore.setUserInfo(userInfo);
+                evaluationScore.setScore(evaluationScoreDto.getScore());
+                evaluationScore.setComment(evaluationScoreDto.getComment());
+            }
         });
     }
 }
