3b81158b7602fde30419a142e00de68eeb681fc4
TaekGwan Kim <sbe07032@naver.com>
2023-09-19 01:31:01 +0900

    [Update] 평가 결과 조회 완료!!!!

--- geeks-react-2.1.1/src/components/dashboard/cms/all-posts/ResultByUser.js
+++ geeks-react-2.1.1/src/components/dashboard/cms/all-posts/ResultByUser.js
@@ -53,6 +53,7 @@ const ResultByUser = (props) => {
       }
     });
     setGroupUser(Object.values(uniqueMap));
+    getUniqueItemJudgeUser(Object.values(uniqueMap));
   }
 
   useEffect(() => {
@@ -72,6 +73,76 @@ const ResultByUser = (props) => {
     return 'X';
   }
 
+  const [resultItemJudgeUser, setResultItemJudgeUser] = useState([]);
+
+  function getUniqueItemJudgeUser(group) {
+    const uniqueMap = {};
+    group.forEach((user) => {
+      let user_id = user.user_id;
+      judgeList.forEach((judge) => {
+        let judge_id = judge.judge_id;
+        itemList.forEach((item) => {
+          let evaluation_id = item.evaluation_id;
+          itemDetailList[evaluation_id].forEach((itemDetail) => {
+            let evaluation_detail_id = itemDetail.evaluation_detail_id;
+            let res = resultList.find(
+              (result) =>
+                result.evaluation_detail_id === evaluation_detail_id &&
+                result.user_id === user_id &&
+                result.judge_id === judge_id,
+            );
+            if (res) {
+              console.log(res.score);
+              if (!uniqueMap[user_id]) {
+                uniqueMap[user_id] = { sum: res.score };
+              } else {
+                uniqueMap[user_id].sum += res.score;
+              }
+              if (!uniqueMap[user_id][judge_id]) {
+                uniqueMap[user_id][judge_id] = { sum: res.score };
+              } else {
+                uniqueMap[user_id][judge_id].sum += res.score;
+              }
+              if (!uniqueMap[user_id][judge_id][evaluation_id]) {
+                uniqueMap[user_id][judge_id][evaluation_id] = res.score;
+              } else {
+                uniqueMap[user_id][judge_id][evaluation_id] += res.score;
+              }
+            }
+          });
+        });
+      });
+    });
+    setResultItemJudgeUser(uniqueMap);
+  }
+
+  function matchItemJudge(user_id, judge_id, evaluation_id) {
+    try {
+      return resultItemJudgeUser[user_id][judge_id][evaluation_id];
+    } catch (e) {
+      return 0;
+    }
+  }
+
+  function matchItemSum(user_id, judge_id) {
+    try {
+      return resultItemJudgeUser[user_id][judge_id].sum;
+    } catch (e) {
+      return 0;
+    }
+  }
+  function matchUserSum(user_id) {
+    try {
+      return resultItemJudgeUser[user_id].sum;
+    } catch (e) {
+      return 0;
+    }
+  }
+
+  if (!resultItemJudgeUser) {
+    return;
+  }
+  console.log(resultItemJudgeUser);
   return (
     <Fragment>
       <Accordion defaultActiveKey={0} flush>
@@ -81,7 +152,7 @@ const ResultByUser = (props) => {
             return (
               <Accordion.Item eventKey={index} key={index}>
                 <Accordion.Header>
-                  | {userInfo.user_name} | {userInfo.total_score}점
+                  | {userInfo.user_name} | {matchUserSum(user_id)}점
                 </Accordion.Header>
                 <Accordion.Body>
                   <Table className="text-nowrap">
@@ -101,6 +172,7 @@ const ResultByUser = (props) => {
                       {itemList.map((item, itemIndex) => {
                         let evaluation_id = item.evaluation_id;
                         let item_max = item.max;
+                        let sumItem = 0;
                         return (
                           <Fragment key={itemIndex}>
                             <tr>
@@ -109,17 +181,27 @@ const ResultByUser = (props) => {
                               </td>
                               {judgeList.map((judge, judgeIndex) => {
                                 let judge_id = judge.judge_id;
+                                let score = matchItemJudge(
+                                  user_id,
+                                  judge_id,
+                                  evaluation_id,
+                                );
+                                sumItem += score;
                                 return (
                                   <td scope="col" key={judgeIndex}>
                                     <div className="d-flex flex-row gap-1">
-                                      <div className="fw-bold">{item.name}</div>
+                                      <div className="fw-bold">{score}</div>
                                       <div>/{item_max}</div>
                                     </div>
                                   </td>
                                 );
                               })}
-                              <td scope="col" className="fw-bold">
-                                {'받은점수 / 총점'}
+
+                              <td scope="col">
+                                <div className="d-flex flex-row gap-1">
+                                  <div className="fw-bold">{sumItem}</div>
+                                  <div>/{item_max * itemList.length}</div>
+                                </div>
                               </td>
                             </tr>
                             {/* 디테일 항목 점순 */}
@@ -128,27 +210,40 @@ const ResultByUser = (props) => {
                                 let evaluation_detail_id =
                                   itemDetail.evaluation_detail_id;
                                 let item_detail_max = itemDetail.max;
+                                let sumItemDetail = 0;
                                 return (
                                   <tr key={detailIndex}>
                                     <td scope="col">{itemDetail.name}</td>
                                     {judgeList.map((judge, judgeIndex) => {
                                       let judge_id = judge.judge_id;
+                                      let score = findMatchingResult(
+                                        evaluation_detail_id,
+                                        user_id,
+                                        judge_id,
+                                      );
+                                      sumItemDetail += score;
                                       return (
                                         <td scope="col" key={judgeIndex}>
                                           <div className="d-flex flex-row gap-1">
                                             <div className="fw-bold">
-                                              {findMatchingResult(
-                                                evaluation_detail_id,
-                                                user_id,
-                                                judge_id,
-                                              )}
+                                              {score}
                                             </div>
                                             <div>/ {item_detail_max}</div>
                                           </div>
                                         </td>
                                       );
                                     })}
-                                    <td scope="col">{'받은점수/총점'}</td>
+
+                                    <td scope="col">
+                                      <div className="d-flex flex-row gap-1">
+                                        <div className="fw-bold">
+                                          {sumItemDetail}
+                                        </div>
+                                        <div>
+                                          /{item_detail_max * itemList.length}
+                                        </div>
+                                      </div>
+                                    </td>
                                   </tr>
                                 );
                               },
@@ -167,15 +262,15 @@ const ResultByUser = (props) => {
                             <td scope="col" key={judgeIndex}>
                               <div className="d-flex flex-row gap-1">
                                 <div className="fw-bold">
-                                  {findMatchingResult(null, user_id, judge_id)}
+                                  {matchItemSum(user_id, judge_id)}
                                 </div>
-                                <div>/ {userInfo.total_score}</div>
+                                {/* <div>/ {userInfo.total_score}</div> */}
                               </div>
                             </td>
                           );
                         })}
                         <td scope="col" className="fw-bold">
-                          {'받은점수/총점'}
+                          {matchUserSum(user_id)}
                         </td>
                       </tr>
                     </tbody>
